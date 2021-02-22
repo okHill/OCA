f6d81b7860fc0c85983cb99ad43ed8a37e4439ca
okurhilmi <okurhilmi@gmail.com>
2021-02-22 12:45:17 -0600

    third commit

---
+++ src/Q_1_12/Q_9/A.java
@@ -0,0 +1,26 @@
+package Q_1_12.Q_9;
+
+    public class A extends B {
+
+        public A() {    // line n2
+            System.out.println("A ");
+        }
+
+        public static void main(String[] args) {
+            A a = new A();
+        }
+    }
+
+
+    class B extends C {
+        public B() {                   //line n1
+            System.out.println("B");
+        }
+    }
+
+        class C {
+            public C() {
+                System.out.println("C");
+            }
+
+    }
